65f3f359fb271596f2f1918f655f5ca53b84cfbe
Arylmatheous <arylmatheous22@gmail.com>
2024-03-28 00:15:08 +0800

    Create Assignment1.2.cs

---
+++ Assignment1.2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+    class Assignment1_2
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 1;
+            while (num1 <= 100)
+            {
+                if (num1 % 3 == 0 && num1 % 5 == 0)
+                {
+                    Console.WriteLine("Hello Goodbye");
+                }
+                else if (num1 % 3 == 0)
+                {
+                    Console.WriteLine("Hello");
+                }
+                else if (num1 % 5 == 0)
+                {
+                    Console.WriteLine("Goodbye");
+                }
+                else
+                {
+                    Console.WriteLine(num1);
+                }
+                num1++;
+
+            }
+        }
+    }
+
+
